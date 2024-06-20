a8d6bc39f24e4820ee55e1dbe3ca2a855a03f3b8
Trevor <hanmingbin@sinosoft.com.cn>
2024-06-20 12:04:05 +0800

    优化路由

--- react-vite/src/router/index.tsx
+++ react-vite/src/router/index.tsx
@@ -2,10 +2,19 @@ import { lazy } from 'react'
 import { RouteObject } from 'react-router-dom'
 const Index = lazy(() => import('../pages/home/index.tsx'))
 const Text = lazy(() => import('../pages/text/index.tsx'))
+import ErrorBoundary from '@/components/ErrorBoundary'
+import NotFound from '@/components/NotFound'
 
 const Router: RouteObject[] = [
+  /* {
+      path: '/',
+      element: <Index />,
+      loader:'' // 路由守卫
+      errorElement: '' // 错误处理
+  },*/
   { path: '/', element: <Index /> },
-  { path: '/text', element: <Text /> }
+  { path: '/text', element: <Text />, errorElement: <ErrorBoundary /> },
+  { path: '*', element: <NotFound /> }
 ]
 
 export default Router
