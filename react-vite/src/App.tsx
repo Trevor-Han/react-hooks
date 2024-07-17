8a32e5f9c74e236f44849fd21e49889bdddba0e8
Trevor <hanmingbin@sinosoft.com.cn>
2024-07-17 09:37:47 +0800

    新增非工作台判断逻辑

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
-import { useRoutes } from 'react-router-dom'
+import { useRoutes, useLocation } from 'react-router-dom'
 import routes from '@/router'
+import fullSceneRouter from '@/router/fullSceneRouter'
 import { ConfigProvider, theme } from 'antd'
 import { useSelector } from 'react-redux'
 import type { RootState } from '@/store'
@@ -9,19 +10,28 @@ import 'antd/dist/reset.css'
 import '@/assets/styles/main.css'
 import '@/assets/styles/responsive.css'
 
+function FullScene() {
+  return <div>
+    {useRoutes(fullSceneRouter)}
+  </div>
+}
+function MainScene() {
+  return <Main>
+    <div className='main'>
+      {useRoutes(routes)}
+    </div>
+  </Main>
+}
 function App() {
   const { defaultAlgorithm, darkAlgorithm } = theme
   const { algorithm } = useSelector((state:RootState) => state.theme)
+  const location = useLocation()
+  const pathNameList = fullSceneRouter.map(item => item.path)
 
   return <div className='App'>
     <ConfigProvider theme={{ algorithm: algorithm === 'light' ? defaultAlgorithm : darkAlgorithm }}>
       <Suspense fallback='Loading...'>
-        <Main>
-          <div className='main'>
-            {useRoutes(routes)}
-          </div>
-        </Main>
-
+        {pathNameList.includes(location.pathname) ? <FullScene /> : <MainScene />}
       </Suspense>
     </ConfigProvider>
   </div>
---
+++ react-vite/src/router/fullSceneRouter.tsx
@@ -0,0 +1,11 @@
+import { lazy } from 'react'
+const Three = lazy(() => import('../pages/three/ThreeContainer.tsx'))
+const SignIn = lazy(() => import('../pages/SignIn/index.tsx'))
+
+const fullSceneRouter = [
+
+  { path: '/three', element: <Three/>, key: 'three' },
+  { path: '/sign-in', element: <SignIn />, auth: true, name: 'signIn' },
+
+]
+export default fullSceneRouter
